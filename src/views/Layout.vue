c4f9597e8585eb12f5b0e4a28d9e33635320a380
clozz-lin <885204655@qq.com>
2022-09-20 18:42:28 +0800

    写到了首页-头部分类导航组件

---
+++ src/views/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav><AppTopnav /></nav>
+  <header><AppHeader /></header>
+  <main>
+    <!-- 二级路由 -->
+    <router-view></router-view>
+  </main>
+  <footer><AppFooter /></footer>
+</template>
+
+<script>
+import AppTopnav from '@/components/app-topnav'
+import AppHeader from '@/components/app-header'
+import AppFooter from '@/components/app-footer'
+export default {
+  name: 'xtx-layout',
+  components: { AppTopnav, AppHeader, AppFooter }
+}
+</script>
+
+<style scoped lang="less">
+main {
+  min-height: 600px;
+}
+</style>
